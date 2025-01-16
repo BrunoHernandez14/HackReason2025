d2a4ec2666fbd342ff88f0a76af770f1fc1d05b5
hadikhan5 <142957650+hadikhan5@users.noreply.github.com>
2025-01-16 14:34:44 -0600

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,5 +1,38 @@
 :- dynamic illness/1, symptoms/2.
 
+illness('Period Cramps'). symptoms('Period Cramps', 
+    ['Pain in the lower abdomen', 'Pain in the lower back', 'Pain in the thighs', 'Nausea', 'Vomiting', 'Fatigue', 'Headache']).
+    
+illness('Fevers'). symptoms('Fevers', 
+    ['High Fever', 'Sweating', 'Chills', 'Shivering', 'Weakness']).
+    
+illness('Diabetes'). symptoms('Diabetes', 
+    ['Increased thirst', 'Frequent urination', 'Extreme hunger', 'Fatigue', 'Blurred vision']).
+    
+illness('Migraine'). symptoms('Migraine', 
+    ['Severe headache', 'Nausea', 'Sensitivity to light', 'Pulsating pain', 'Fatigue']).
+    
+illness('Hypertension'). symptoms('Hypertension', 
+    ['Headache', 'Shortness of breath', 'Nosebleeds', 'Dizziness', 'Chest pain']).
+    
+illness('Anemia'). symptoms('Anemia', 
+    ['Fatigue', 'Pale skin', 'Irregular heartbeats', 'Shortness of breath', 'Dizziness']).
+    
+illness('Asthma'). symptoms('Asthma', 
+    ['Shortness of breath', 'Chest tightness', 'Wheezing', 'Coughing', 'Anxiety']).
+    
+illness('Shingles'). symptoms('Shingles', 
+    ['Pain', 'Burning', 'Tingling', 'Red rash', 'Fever']).
+    
+illness('Acid Reflux'). symptoms('Acid Reflux', 
+    ['Heartburn', 'Chest pain', 'Difficulty swallowing', 'Regurgitation', 'Nausea']).
+    
+illness('Arthritis'). symptoms('Arthritis', 
+    ['Joint pain', 'Stiffness', 'Swelling', 'Fatigue', 'Tenderness']).
+    
+illness('Bronchitis'). symptoms('Bronchitis', 
+    ['Cough', 'Production of mucus', 'Fatigue', 'Shortness of breath', 'Chest discomfort']).
+    
 % Illness List
     illness('Period Cramps').
     illness('Fevers').
